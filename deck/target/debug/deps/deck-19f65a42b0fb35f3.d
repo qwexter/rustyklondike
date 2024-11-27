af4c7b6eea7611171f06717a819d7249dd780db8
A. Bubnov <69006636+qwexter@users.noreply.github.com>
2024-11-27 18:07:19 +0400

    Remove target subfolder folder from tracking

--- deck/target/debug/deps/deck-19f65a42b0fb35f3.d
+++
@@ -1,7 +0,0 @@
-/mnt/c/Users/AlexBubn/Projects/klondike/deck/target/debug/deps/libdeck-19f65a42b0fb35f3.rmeta: src/lib.rs
-
-/mnt/c/Users/AlexBubn/Projects/klondike/deck/target/debug/deps/libdeck-19f65a42b0fb35f3.rlib: src/lib.rs
-
-/mnt/c/Users/AlexBubn/Projects/klondike/deck/target/debug/deps/deck-19f65a42b0fb35f3.d: src/lib.rs
-
-src/lib.rs:
--- deck/target/debug/deps/deck-c9cfecec991a628d.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/AlexBubn/Projects/klondike/deck/target/debug/deps/deck-c9cfecec991a628d: src/lib.rs
-
-/mnt/c/Users/AlexBubn/Projects/klondike/deck/target/debug/deps/deck-c9cfecec991a628d.d: src/lib.rs
-
-src/lib.rs:
